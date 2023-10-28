831c7b6bf72bd691a08c7b5caec327e0b44aa9ee
27733 <27733@DESKTOP-2BB70CT>
2023-10-29 00:15:55 +0800

    添加项目文件。

---
+++ Project1/test.cpp
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <conio.h>
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+void main()
+{
+	float a;
+	a = sin(3.14/6);
+	printf("%f", a);
+	system("pause");
+}
